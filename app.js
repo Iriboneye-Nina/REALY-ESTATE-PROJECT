44dea6e9e157df4dd83ab01942a6d7651e9dbe8f
Iriboneye-Nina <ninairiboneye@gmail.com>
2023-10-26 09:32:01 +0200

    other code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener("scroll", function(){
     var nav = document.querySelector("nav")
-    nav.classList.toggle("sticky", window.scrollY, 0)
+    nav.classList.toggle("sticky", window.scrollY > 0);
 })
 var menu = document.querySelector('.menu');
 var menuBtn = document.querySelector('.menu-btn')
